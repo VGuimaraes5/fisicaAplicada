bed047079bc0c59de18f15641873d04f5d5b1aa9
Vinicius Guimarães <vinicius.guimaraes5@fatec.sp.gov.br>
2021-04-17 09:10:42 -0300

    atualiza

---
+++ Assets/Scripts/centroMassa/rotacao.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotacao : MonoBehaviour
+{
+    public Vector3 torque;
+
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.AddTorque(torque);
+    }
+}
