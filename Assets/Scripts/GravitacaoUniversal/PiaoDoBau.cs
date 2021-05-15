af2d2d8033448537a63039ea66ac54bc8eb3f766
Vinicius Guimarães <vinicius.guimaraes5@fatec.sp.gov.br>
2021-05-15 12:05:05 -0300

    Gravitacional

---
+++ Assets/Scripts/GravitacaoUniversal/PiaoDoBau.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiaoDoBau : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 m_VelocityAngular;
+
+    private Vector3 m_Rotation;
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        m_Rotation = m_VelocityAngular * Time.fixedDeltaTime;
+        transform.Rotate(m_Rotation, Space.Self);
+    }
+}
